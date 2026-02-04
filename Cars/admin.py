192a364194954aa3cf4266c88e608666fde71aae
JoaoGFS <165217153+JoaoGFurtadoSousa@users.noreply.github.com>
2026-02-04 10:23:33 -0300

    chore(Cars): Ajuste no admin da APP cars

--- Cars/admin.py
+++ Cars/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from Cars.models import Cars
 # Register your models here.
+
+@admin.register(Cars)
+class CarsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'ano', 'preco', 'marca', )
+    search_fields = ('nome', )
