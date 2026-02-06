4f77844d7feaceeb2770cc57f9a384a77b631b0b
JoaoG <165217153+JoaoGFurtadoSousa@users.noreply.github.com>
2026-02-06 15:26:02 -0300

    feat(autenticacao): Logica de login e cadastro completa

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password, check_password
+from django.http import HttpResponse
 import os
 
 def cadastro(request): 
@@ -9,13 +10,14 @@ def cadastro(request):
 
 
 def salvar_pessoa_no_banco(request):
-    nome = request.POST.get('nome')
-    email = request.POST.get('email')
-    password = request.POST.get('password')
-    print(password)
-    usuario = User.objects.create_user(username = nome, email = email, password= password)
+    if request.POST:
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        print(password)
+        usuario = User.objects.create_user(username = nome, email = email, password= password)
+        return render(request, 'login.html')
     return render(request, 'forms.html')
-
 
 def lista_todas_as_pessoas(request):
     lista_de_pessoas = User.objects.all()
@@ -27,12 +29,20 @@ def login(request):
         password = request.POST.get('password')
 
         if email != None and password != None:
-            usuario = User.objects.get(email= email)
-            if usuario:
-                password = check_password(usuario.password, password)
-                if usuario.password == password:
-                    print('boa')
+            try:
+                usuario = User.objects.get(email= email)
+                print(password)
+                print(usuario.password)
+                print(f'Senha digitada: {password}')
+                if check_password(password, usuario.password) == True:
+                    print(f'entrou: {usuario.password}')
                     return render(request, 'home.html')
-    return render(request, 'login.html')
+                print('senha incorreta')
+                return render(request, 'login.html')
+            except User.DoesNotExist:
+                return HttpResponse({'error':'User not found'}, status= 404)
+        
+    else:
+        print('Requisicao do GET')
+        return render(request, 'login.html') 
 
-    
